d47dc9357c1e7d92dd07082f06de3d226324607c
zagig <zagigalka_60@mail.ru>
2020-12-27 16:06:31 +0300

    add part for windows for compil

---
+++ include/shared_ptr.hpp
@@ -0,0 +1,155 @@
+//
+// Created by zagig on 27.12.2020.
+//
+
+#ifndef LAB_03_SHARED_PTR_SHARED_PTR_HPP
+#define LAB_03_SHARED_PTR_SHARED_PTR_HPP
+
+#include <atomic>
+#include <iostream>
+#include <utility>
+
+template <typename T>
+class shared_ptr {
+ public:
+  SharedPtr(){
+      ptr= nullptr;
+      count_ptr=nullptr;
+  };
+
+  explicit SharedPtr(T* ptr){
+    ptr=point;
+      if (point==nullptr){
+        count_ptr = nullptr;
+      } else {
+        count_ptr = new std::atomic_uint ;
+        *count_ptr=1;
+      }
+  };
+
+  SharedPtr(const SharedPtr& r){
+      ptr=r.ptr;
+      count_ptr=r.count_ptr;
+      if (ptr){
+        *count_ptr = *count_ptr+1;
+      } else {
+        *count_ptr=nullptr;
+      }
+  };
+
+  SharedPtr(SharedPtr&& r){
+      ptr=r.ptr;
+      count_ptr=r.count_ptr;
+      r.ptr=nullptr;
+      r.count_ptr=nullptr;
+  };
+
+  ~SharedPtr(){
+      if (coun_ptr){
+        *count_ptr==1{
+          delete ptr;
+          delete count_ptr;
+        } else {
+          *count_ptr=count_ptr-1;
+        }
+      }
+      ptr=nullptr;
+      count_ptr=nullptr;
+  };
+
+  auto operator=(const SharedPtr& r) -> SharedPtr&{
+      if (this !=&r) {
+      ptr = r.ptr;
+      count_ptr = r.count_ptr;
+      if (ptr) {
+        *count_ptr = *count_ptr + 1;
+      } else {
+        count_ptr = nullptr;
+      }
+    }
+    return *this;
+  };
+
+  auto operator=(SharedPtr&& r) -> SharedPtr&{
+      if(this!=&r){
+        ptr=r.ptr;
+        count_ptr=r.count_ptr;
+      }
+      return this;
+  };
+
+
+  // проверяет, указывает ли указатель на объект
+  operator bool() const{
+      if (ptr)
+        return true;
+      else
+        return false;
+  };
+
+  auto operator*() const -> T&{
+      if(ptr)
+        return *ptr;
+      else
+        return nullptr;
+  };
+
+  auto operator->() const -> T*{
+      return ptr;
+  };
+
+  auto get() -> T*{
+  return ptr;
+  };
+
+  void reset(){
+      if(*count_ptr==1){
+        delete ptr;
+        delete count_ptr;
+      }else{
+        *count_ptr=*count_ptr-1;
+      }
+      ptr=nullptr;
+      count_ptr=nullptr;
+  };
+
+  void reset(T* ptr){
+      if(*count_ptr==1){
+        delete ptr;
+        delete count_ptr;
+      }else{
+        *count_ptr=*count_ptr-1;
+      }
+      ptr=point;
+      if(point==nullptr){
+        count_ptr=nullptr;
+      }else{
+        count_ptr=new std::atomic_uint;
+        *count_ptr=1;
+      }
+  };
+
+  void swap(SharedPtr& r){
+      T*tmp_ptr(std::move(r.ptr));
+      r.ptr=std::move(ptr);
+      ptr=std::move(tmp_ptr);
+      std::atomic_uint* tmp_count_ptr=r.count_ptr;
+      r.count_ptr=count_ptr;
+      count_ptr=tmp_count_ptr;
+  };
+  // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+
+  auto use_count() const -> size_t{
+      if(count_ptr){
+        return *count_ptr;
+      }else{
+        return 0;
+      }
+  };
+
+ private:
+  T* ptr;
+  std::atomic_uint* count_ptr;
+};
+
+#endif  // LAB_03_SHARED_PTR_SHARED_PTR_HPP
